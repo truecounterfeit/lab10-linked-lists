b2958ff5824e2993663ebb308de69f2afddfbe40
Kelati Andemichael <Kelati@Macbooks-MacBook-Pro-2.local>
2018-02-04 14:04:18 -0800

    remove protoype, tests

--- test/list-test.js
+++ test/list-test.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const expect = require('expect');
+const LinkedList = require('../lib/linkedlist.js');
+const SLList = require('../lib/list.js');
+
+describe('append module', function() {
+
+it('should create new element if no list exists', function(){
+
+let testList = new LinkedList();
+testList.append(3);
+
+expect(testList).toEqual({
+  head: {
+    value: 3,
+    next: null,
+    },
+  });
+});
+
+it('should add a new element to the end of the list', function(){
+
+        let testList2 = new LinkedList();
+        testList2.append(3);
+        testList2.append(4);
+        expect(testList2).toEqual({
+             head: {
+             value: 3,
+             next: {
+               value: 4,
+               next: null,
+             },
+           },
+         });
+
+       });
+
+     });
